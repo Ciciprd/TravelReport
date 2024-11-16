d4fdf79b3f54af1962cd6e607a64f324233185c2
Kamcici <128805746+Ciciprd@users.noreply.github.com>
2024-11-16 23:08:12 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const cacheName = 'my-pwa-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+];
+
+// Install service worker and cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+// Fetch from cache first, then from network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+  );
+});
+
+// Activate service worker and clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [cacheName];
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (!cacheWhitelist.includes(cacheName)) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+  );
+});
